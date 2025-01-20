3bd70f99555b6ac0daf95c845f7d67833260e37f
mtyd2008 <tahaydandya@gmail.com>
2025-01-20 21:33:45 +0500

    Google authentication complete

--- app.js
+++ app.js
@@ -1,7 +1,6 @@
-import { onAuthStateChanged , signOut } from "https://www.gstatic.com/firebasejs/11.1.0/firebase-auth.js";
+import { onAuthStateChanged , signOut , GoogleAuthProvider , signInWithPopup} from "https://www.gstatic.com/firebasejs/11.1.0/firebase-auth.js";
 import { auth } from "./fireconfig.js"
 
-const logout = document.querySelector("#logout-btn")
 
 
 onAuthStateChanged(auth,  (user) => {
@@ -14,6 +13,8 @@ onAuthStateChanged(auth,  (user) => {
   }
 });
 
+const logout = document.querySelector("#logout-btn")
+
 logout.addEventListener('click' , ()=>{
     signOut(auth).then(() => {
         console.log('logout sucessfully');
@@ -23,4 +24,26 @@ logout.addEventListener('click' , ()=>{
         console.log(error);
         
       });
+})
+
+const provider = new GoogleAuthProvider();
+const google = document.querySelector("#google-btn")
+
+
+google.addEventListener('click' , ()=>{
+  // console.log('google');
+  
+  signInWithPopup(auth, provider)
+  .then((result) => {
+    const credential = GoogleAuthProvider.credentialFromResult(result);
+    const token = credential.accessToken;
+    const user = result.user;
+    console.log(token);
+    console.log(user);
+    
+  }).catch((error) => {
+    const errorMessage = error.message;
+    console.log(errorMessage);
+    
+  });
 })
